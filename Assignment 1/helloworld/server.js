e97116a1b832ca5b1953bb18e6aaab42fed7ae96
Nishit Shah <ngshah3@myseneca.ca>
2022-09-15 00:13:49 -0400

    working on two repos

---
+++ Assignment 1/helloworld/server.js
@@ -0,0 +1,11 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
