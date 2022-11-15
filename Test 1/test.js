14e74561e82e47ebcdbb4547cf0038adeccd1505
Nishit Shah <ngshah3@myseneca.ca>
2022-11-15 00:42:06 -0500

    Delete Test 1 directory

--- Test 1/test.js
+++
@@ -1,9 +0,0 @@
-var counter = require("./counter");
-
-//TESTING
-counter.multiply(15);
-counter.divide(2);
-console.log(counter.value());
-counter.multiply(5);
-counter.divide(2);
-console.log(counter.value());
